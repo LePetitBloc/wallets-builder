14381f1fe8cf6c211ec9da579e200196419246a7
lutangar <johan.dufour@gmail.com>
2018-06-15 18:06:02 +0200

    handle tags and branches

--- buildArgs.js
+++ buildArgs.js
@@ -5,13 +5,14 @@ const buildArgs = (wallet) => {
         CONFIG_FILE: wallet.configFile,
         PORT: wallet.mainnetPort,
         RPC_PORT: wallet.mainRpcPort,
-        REPOSITORY: `${wallet.walletSrc}.git`,
+        REPOSITORY: `${wallet.repository}.git`,
+        REF: wallet.tag ? `tags/${wallet.tag}` : wallet.branch ? `remotes/origin/${wallet.branch}` : 'master',
     };
 
     return Object
         .keys(argsMap)
         .reduce((args, key) => args.concat('--build-arg').concat(`${key}=${argsMap[key]}`), [])
-        ;
+    ;
 };
 
 module.exports = buildArgs;
